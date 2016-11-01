3966db01914bd3c415e27801207bce151fe0b0be
Antoine Brossault <contact@antoinebrossault.com>
2016-11-01 10:37:33 +0000

    fix error file_get_content

--- inc/css-async.php
+++ inc/css-async.php
@@ -1,5 +1,5 @@
 <!-- inject critical CSS -->
-<style><?= file_get_contents(BASEURL."css/critical.css");?></style>
+<style><?php include("css/critical.css");?></style>
 <!-- load full CSS stylesheet in async -->
 <link rel="preload" href="<?= BASEURL;?>css/app.css" as="style" onload="this.rel='stylesheet'">
 <noscript><link rel="stylesheet" href="<?= BASEURL;?>css/app.css"></noscript>
