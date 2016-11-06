d1490b4eb7e9800abd68408e80591c1fa747b1ad
Antoine Brossault <contact@antoinebrossault.com>
2016-11-06 09:33:56 +0100

    fix sprite

--- index.php
+++ index.php
@@ -22,46 +22,7 @@
 			<p>Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam.Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam.Lorem ipsum dolor sit met, qui at desert mandamus, adduce ullum apeirian mea at. Eu mel vide saltando vituperata, sonet quidam deterruisset te qui. Te cum vivendum explicate abhorrent. Id venom argumentum vel. Ut lorem bocent hendrerit eam.</p>
 		<?php endfor; ?>
 
-
-
-
 	</div> <!--container -->
 
-	<footer>
 
-		<div class="social-group">
-
-			<?php if(SPRITE !== "false"): ?>
-				<a href="#"><i class="icon-github"></i></a>
-				<a href="#"><i class="icon-fork"></i></a>
-				<a href="#"><i class="icon-plus"></i></a>
-				<a href="#"><i class="icon-twitter"></i></a>
-				<a href="#"><i class="icon-equals"></i></a>
-			<?php else: ?>
-				<a href="#"><img src="img/sprites/raw/github.png"></i></a>
-				<a href="#"><img src="img/sprites/raw/fork.png"></a>
-				<a href="#"><img src="img/sprites/raw/plus.png"></i></a>
-				<a href="#"><img src="img/sprites/raw/twitter.png"></a>
-				<a href="#"><img src="img/sprites/raw/equals.png"></a>
-			<?php endif;?>
-		</div>
-
-
-	</footer>
-
-
-
-	<!-- SCRIPT FOOTER -->
-	<?php (SCRIPT == "footer") ? include('inc/scripts.php') : ""; ?>
-
-
-	<!-- CSS async -->
-	<?php if(CSS == "inline"): ?>
-		<script>loadCSS( "<?= BASEURL;?>css/app.css" );</script>
-	<?php endif;?>
-
-
-
-
-</body>
-</html>
+	<?php include('footer.php');?>
