04c683852fcc7eb214fc3a8936f820ad2368a5cb
Antoine Brossault <abrossault@abrossault-macbookair.roam.corp.google.com>
2016-11-08 23:30:17 +0000

    Fix YT experience condition

--- inc/video-yt.php
+++ inc/video-yt.php
@@ -1,4 +1,4 @@
-<?php if(LAZYLOADING == "true"): ?>
+<?php if(LAZYLOADING == "true" && NOIFRAME == "false"): ?>
   <div class="bg-video lazyload"  data-bgset="http://lorempicsum.com/simpsons/200/200/1 200w, http://lorempicsum.com/simpsons/300/300/1 300w, http://lorempicsum.com/simpsons/400/300/1 400w, http://lorempicsum.com/simpsons/768/400/1 700w" data-sizes="auto">
     <a id="banner-player" href="https://www.youtube.com/watch?v=I4vX-twze9I">
       <img class="player-btn lazyload"  data-src="img/home-banner-promo-player.png">
